4ebddf3a079060020fe83416e4c17cfc6bf2e587
Kevin Zhang <zhangyongjiang@yahoo.com>
2014-08-12 16:56:02 -0700

    code refactoring

---
+++ src/main/java/common/db/shard/ShardedDataSource.java
@@ -0,0 +1,13 @@
+package common.db.shard;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+public interface ShardedDataSource {
+	int getTotalShards();
+	DataSource getDataSource(int shard);
+	NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(int shardId);
+	JdbcTemplate getJdbcTemplate(int shardId);
+}
---
+++ src/main/java/common/db/shard/ShardedDataSourceImpl.java
@@ -0,0 +1,48 @@
+package common.db.shard;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+public class ShardedDataSourceImpl implements ShardedDataSource {
+	private Map<Integer, JdbcTemplate> jdbcTemplates;
+	private Map<Integer, NamedParameterJdbcTemplate> namedJdbcTemplates;
+	private List<DataSource> dataSources;
+
+	@Override
+	public int getTotalShards() {
+		return dataSources.size();
+	}
+
+	@Override
+	public DataSource getDataSource(int shard) {
+		return dataSources.get(shard);
+	}
+
+	public synchronized NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(int shardId) {
+		NamedParameterJdbcTemplate template = namedJdbcTemplates.get(shardId);
+		if(template == null) {
+			template = new NamedParameterJdbcTemplate(getDataSource(shardId));
+			namedJdbcTemplates.put(shardId, template);
+		}
+		return template;
+	}
+	
+	public synchronized JdbcTemplate getJdbcTemplate(int shardId) {
+		JdbcTemplate template = jdbcTemplates.get(shardId);
+		if(template == null) {
+			template = new JdbcTemplate(getDataSource(shardId));
+			jdbcTemplates.put(shardId, template);
+		}
+		return template;
+	}
+
+	public void setDataSources(List<DataSource> dataSources) {
+		this.dataSources = dataSources;
+	}
+
+}
